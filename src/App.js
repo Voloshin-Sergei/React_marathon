4cd5701566e8d8b35216a5bce5ef68d02cca4382
Voloshin_Sergey <4voloshin@gmail.com>
2021-01-28 21:52:38 +0300

    fix: mentor comments

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ const App = () => {
   return (
     <>
       <Header title={'This is Pokemon Card Game'} descr={'Simple Triple Triad Card Game'} />
-      <Layout id={1} title={'First layout'} descr={'Description'} urlBg={urlBg} colorBg={''} />
-      <Layout id={2} title={'Second layout'} descr={'Description'} urlBg={''} colorBg={'#ffa401'} />
-      <Layout id={3} title={'Third layout'} descr={'Description'} urlBg={urlBg} colorBg={''} />
+      <Layout id={1} title={'First layout'} descr={'Description'} urlBg={urlBg} />
+      <Layout id={2} title={'Second layout'} descr={'Description'} colorBg={'#ffa401'} />
+      <Layout id={3} title={'Third layout'} descr={'Description'} urlBg={urlBg} />
       <Footer />
     </>
   );
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,11 +2,11 @@ import s from './style.module.css';
 //import urlBg from './assets/bg1.jpg';
 
 const Layout = ({ id, title, descr, urlBg, colorBg }) => {
+  const styleSection = colorBg
+    ? { backgroundColor: colorBg }
+    : { backgroundImage: `url(${urlBg})` };
   return (
-    <section
-      className={s.root}
-      id={id}
-      style={colorBg ? { backgroundColor: colorBg } : { backgroundImage: `url(${urlBg})` }}>
+    <section className={s.root} id={id} style={styleSection}>
       <div className={s.wrapper}>
         <article>
           <div className={s.title}>
